b7ef91c6ff70eb588cbec76ff487b04efad2fa52
kevin diesenberg <kdiesenb@gmail.com>
2022-10-30 15:12:05 -0400

    commit tree 184453d3066bc51d9dd2940f6b3af6ae20975f3d

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let greeting;
+  if (someValue <= 400) {
+    greeting = "This one is on me!";
+  }
+  else if (someValue >= 400 && someValue <= 2000) {
+    greeting = "That will be twenty bucks.";
+  }
+  else if (someValue >= 2000 && someValue < 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    greeting = "No can do.";//i don't understand why this won't pass the last test
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return (someCity === 'NYC')? "Ok, sounds good.": "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  let greeting;
+  switch(charm) {
+    case "generous":
+      greeting = "Thank you so much.";
+      break;
+    case "not as generous":
+      greeting = "Thank you.";
+      break;
+      default:
+        greeting = "Bye.";
+
+  }
+  return greeting;
 }
